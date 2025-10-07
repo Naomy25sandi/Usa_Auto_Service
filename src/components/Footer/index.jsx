a8cccc5685ab6fcd5ee77ca39ec5a0da16878e0c
Javier348solis <javiersolisretana@gmail.com>
2025-10-07 10:05:39 -0600

    Se creo el footer y sus css

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,40 @@
+import "../../Styles/footer.css";
+import { FaWhatsapp, FaInstagram, FaFacebook } from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <p>© {new Date().getFullYear()} USA Auto-Service – Todos los derechos reservados</p>
+        <p>Lake Worth, Florida, Estados Unidos</p>
+
+        <div className="footer-socials">
+          <a
+            href="https://wa.me/0017543663591"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-link whatsapp"
+          >
+            <FaWhatsapp />
+          </a>
+          <a
+            href="https://www.instagram.com/usa_auto_service_imp_llc"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-link instagram"
+          >
+            <FaInstagram />
+          </a>
+          <a
+            href="https://www.facebook.com/share/19A9Xapx5q/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-link facebook"
+          >
+            <FaFacebook />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
