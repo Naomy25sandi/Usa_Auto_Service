7df7b4e019d03c48b9fe621246036b538179ae33
Javier348solis <javiersolisretana@gmail.com>
2026-04-27 10:29:02 -0600

    Se elimino la descripcion y foto de Noe

--- src/containers/Nosotros/index.jsx
+++ src/containers/Nosotros/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../../styles/nosotros.css";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
-import Foto from "../../assets/foto Noe.jpeg";
+// import Foto from "../../assets/foto Noe.jpeg";
 import { useTranslation } from "react-i18next";
 
 const Nosotros = () => {
@@ -19,18 +19,8 @@ const Nosotros = () => {
 
           <p className="section-description">{t("about.description")}</p>
 
-          <div className="fundadores">
-            <div className="persona-wrapper">
-              <div className="persona">
-                {/* <img src={Foto} alt={t("about.founder.name")} /> */}
-                {/* <p>
-                  {t("about.founder.name")} <br />
-                  <span className="role">{t("about.founder.role")}</span>
-                </p> */}
-                {/* <p className="bio">{t("about.founder.bio")}</p> */}
-              </div>
-            </div>
-          </div>
+         
+       
         </div>
       </section>
       <Footer />
